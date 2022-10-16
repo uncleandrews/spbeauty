6fe23292228c08b255b64212b644ac87e57c3536
Andrey <aperminov@me.com>
2022-10-16 13:36:57 +0000

    Update 12 files

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -1,16 +1,32 @@
 import PrimaryButton from "../../../../../Components/PrimaryButton";
 import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
-function Addresses() {
+import styled from "styled-components"
+
+
+function Addresses(props) {
+    let minWidth = props.minWidth === undefined ? '453px' : props.minWidth;
+    const AddressBlock = styled.div`
+    border-radius: 8px;
+    border: solid rgba(100,116,139,0.12) 1px;
+    border-opacity: 0.12;
+    margin-left: 48px;
+    margin-right: 72px;
+    min-width: ${minWidth};
+`;
+    
     return (
-        <div className='d-inline-flex flex-column gap-2 mt-4'>
-            <div className='text-rg'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-3'>
+        <AddressBlock className='d-inline-flex flex-column justify-content-between p-4'>
+            {props.children}
+            <div className='d-inline-flex flex-column'>
+                <div className='title mb-4'>Адреса заведений</div>
+                <div className='d-inline-flex flex-column gap-2'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                <PrimaryButton>Добавить филиал</PrimaryButton>
+                </div>
             </div>
-        </div>
+            <SecondaryButton>Добавить филиал</SecondaryButton>
+        </AddressBlock>
     );
 }
 
--- src/Layouts/Page/AboutCompany/Components/Heading.js
+++ src/Layouts/Page/AboutCompany/Components/Heading.js
@@ -1,11 +1,19 @@
 import PrimaryButton from "../../../../Components/PrimaryButton";
 import SecondaryButton from "../../../../Components/SecondaryButton";
+import styled from "styled-components";
+
+const HeadingBlock = styled.div`
+    margin-left: 72px;
+    margin-right: 72px;
+    margin-top: 64px;
+`;
+
 function Heading() {
     return (
-        <div className='d-flex flex-row justify-content-between mt-5'>
+        <HeadingBlock className='d-flex flex-row justify-content-between'>
             <h1 className='large-title'>Данные о компании</h1>
-            <PrimaryButton>Закрыть запись</PrimaryButton>
-        </div>
+            <SecondaryButton>Закрыть запись</SecondaryButton>
+        </HeadingBlock>
     );
 }
 
